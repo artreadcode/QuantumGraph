6fd41929cdbeab91f0ea23f011aba3b4f387fbcf
Astryd Park <astryd@mothquantum.com>
2026-09-10 11:49:33 +0100

    Added a function for mapping audio to circuit operation

--- microquantumgraph/microquantumgraph.py
+++ microquantumgraph/microquantumgraph.py
@@ -35,10 +35,38 @@ class MicroQuantumGraph():
         return backend
     
     def get_bloch(self, qubit):
+        ''' Returns per-qubit values '''
+        
         expectation = {}
         full_pauli = ['I'] * self.qc.num_qubits
         for pauli in ['X', 'Y', 'Z']:
             full_pauli[qubit] = pauli
             expectation[pauli] = self.pauli_decomp[''.join(full_pauli)]
         return expectation
+
+    def get_relationship(self, qubit0, qubit1):
+        ''' Returns per-pair values '''
+        relationship = []
+        
+        return relationship
+
+
+# This totally new module function will define
+# how audio values will create and modify quantum circuit
+def build_circuit(features, num_qubits, pairs):
+    ''' 
+    Map audio features to a micromoth circuit.
+    
+    Attributes:
+    - features: dict with 'low', 'mid', 'high', 'rms', each 0..1.
+        - low, mid, high: per-qubit rotation angles (More entangling gates richer per-pair values but per-qubit will be less impactful.)
+        - rms: Overall level so use it as a gate on entanglement so silence gives a calm state.
+    - num_qubits: number of qubits (graph nodes)
+    - pairs: list of [j, k] qubit pairs to entangle.
+    
+    Returns a MicroMoth QuantumCircuit.
+    '''
+    qc = QuantumCircuit(num_qubits)
+    # TODO
+    return qc
         
